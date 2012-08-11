664bec18f390c14c4f4fe63bc34cfba1624d40de
Chylli <chylli.email@gmail.com>
2012-08-11 09:11:06 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskobserve::Application.config.secret_token = '88b1d40958d1ec99b73d11d0d0503f8a60dec4802a6fedd2d4b346c17b589530c2f9619a2c1bc86e2c70928f58770ca976238c4cb3c55aa95a434f84fe7bcc90'
