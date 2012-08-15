975dfffa52f62bab6f42a5ad2a6df6abf9d8e15b
Chylli <chylli.email@gmail.com>
2012-08-15 13:49:05 +0800

    add filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authorize
+
+  protected
+
+  def authorize
+    if session[:user_id]
+    then
+      render(:text => "Welcome #{session[:user_id]}")
+    else
+      redirect_to login_url, :notice => "Please Log in"
+    end
+    
+    
+  end
+
+
 end
