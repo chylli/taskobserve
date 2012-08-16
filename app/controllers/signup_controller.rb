3060421c882b45b0bfd34b690a95ba724c151399
Chylli <chylli.email@gmail.com>
2012-08-16 10:34:23 +0800

    signup not work. will do it later

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -4,5 +4,17 @@ class SignupController < ApplicationController
   end
 
   def create
+    user = {}
+    user["name"] = params[:name]
+    user["login"] = params[:login]
+    user["email"] = params[:email]
+    user["password"] = params[:password]
+    user["password_confirmation"] = params[:password_confirmation]
+    post_param = {"user" => user}
+    puts post_param.to_json
+    ret = RestClient.post "https://railstest.tasker.ly/users.json", post_param.to_json, :content_type => :json, :accept => :json
+    render(:text => JSON.parse(ret))
+    #render(:text => post_param.to_json)
+
   end
 end
