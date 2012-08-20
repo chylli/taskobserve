22c7eacb4c748399d2fb560254f579ee3dcf6f47
Chylli <chylli.email@gmail.com>
2012-08-20 15:10:47 +0800

    signup ok

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -6,22 +6,26 @@ class SignupController < ApplicationController
     res = RestClient.get url, {:accept => :json}
     res = JSON.parse res
     @captcha = res["captcha"]
-    puts "x" * 80
-    puts @captcha
-
+    @tmp_user = flash[:tmp_user] || {}
   end
 
   def create
-    user = {}
-    user["name"] = params[:name]
-    user["login"] = params[:login]
-    user["email"] = params[:email]
-    user["password"] = params[:password]
-    user["password_confirmation"] = params[:password_confirmation]
-    post_param = {"user" => user}
-    puts post_param.to_json
-    ret = RestClient.post "https://railstest.tasker.ly/users.json", post_param.to_json, :content_type => :json, :accept => :json
-    render(:text => JSON.parse(ret))
+    @tmp_user = params[:user]
+    post_param = {"user" => @tmp_user}.to_json
+    ret = RestClient.post("https://railstest.tasker.ly/users.json", post_param, :content_type => :json, :accept => :json) {|response| response}
+    
+    case ret.code
+    when 200
+      user = User.new(:name => @tmp_user["name"], :password => @tmp_user["password"])
+      user.save
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      error = JSON.parse(ret)
+      flash[:tmp_user] = @tmp_user
+      flash[:error] = error["errors"]
+      redirect_to(:action => 'new')
+    end
     #render(:text => post_param.to_json)
 
   end
