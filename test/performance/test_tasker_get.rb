1c08a03e6f25321f1b867d7e2984c19e886127f4
Chylli <chylli.email@gmail.com>
2012-09-11 23:18:01 +0800

    last activity view changed

--- test/performance/test_tasker_get.rb
+++ test/performance/test_tasker_get.rb
@@ -7,7 +7,7 @@ n=20
 tasker = Tasker.new
 
 Benchmark.bm do |x|
-  x.report('usersn') {n.times { tasker.users } }
+  x.report('users') {n.times { tasker.users } }
   x.report('user1801_shared_tags') {n.times {tasker.user_shared_tags(1801)}}
   x.report('current_tasks') {n.times {tasker.user_tasks(1801)}}
   x.report('task320808') {n.times {tasker.task(320808)}}
