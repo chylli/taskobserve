a9e217cb3e64bbbf47edf01e9b9b3375037dbe09
Chylli <chylli.email@gmail.com>
2012-09-11 13:51:04 +0800

    change output

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,8 +3,8 @@ require 'rails/performance_test_help'
 
 class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
-  self.profile_options = { :runs => 20, :metrics => [:wall_time, :memory]
-                           :output => 'tmp/performance', :formats => [:flat] }
+  self.profile_options = { :runs => 20, :metrics => [:wall_time, :memory] }
+  #                         :output => 'tmp/performance', :formats => [:flat] }
 
   def test_membership_user
     get '/membership/1801'
